2d3554c06ede1eb30dddc81750cefade75a910dd
Gabriel S <41161023+souzagab@users.noreply.github.com>
2023-05-28 19:53:31 -0300

    Add datadog (#18)
    * feat: add postgres configuration
    
    * chore: add redis configuration
    
    * feat: add prerun script

---
+++ datadog/prerun.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# # Disable the Datadog Agent based on dyno type
+# if [ "$DYNOTYPE" == "run" ]; then
+#   DISABLE_DATADOG_AGENT="true"
+# fi
+
+# # Disable integrations based on dyno type
+# if [ "$DYNOTYPE" != "web" ]; then
+#   rm -f "$DD_CONF_DIR/conf.d/gunicorn.d/conf.yaml"
+# fi
+
+# Set app version based on HEROKU_SLUG_COMMIT
+if [ -n "$HEROKU_SLUG_COMMIT" ]; then
+  DD_VERSION=$HEROKU_SLUG_COMMIT
+fi
+
+# Install the "ping" community integration
+agent-wrapper integration install -t datadog-ping==1.0.0
+
+# Update the Postgres configuration from above using the Heroku application environment variable
+if [ -n "$DATABASE_URL" ]; then
+  POSTGREGEX='^postgres://([^:]+):([^@]+)@([^:]+):([^/]+)/(.*)$'
+  if [[ $DATABASE_URL =~ $POSTGREGEX ]]; then
+    sed -i "s/<YOUR HOSTNAME>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+    sed -i "s/<YOUR USERNAME>/${BASH_REMATCH[1]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+    sed -i "s/<YOUR PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+    sed -i "s/<YOUR PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+    sed -i "s/<YOUR DBNAME>/${BASH_REMATCH[5]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+  fi
+fi
+
+# Update the Redis configuration from above using the Heroku application environment variable
+if [ -n "$REDIS_URL" ]; then
+  REDISREGEX='rediss?://([^:]*):([^@]+)@([^:]+):([^/]+)$'
+  if [[ $REDIS_URL =~ $REDISREGEX ]]; then
+    sed -i "s/<YOUR_REDIS_HOST>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+    sed -i "s/<YOUR_REDIS_PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+    sed -i "s/<YOUR_REDIS_PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+  fi
+fi
